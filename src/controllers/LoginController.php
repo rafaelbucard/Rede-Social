f9635e729488be1d5f72da061f36e6cdc8df5857
bucard <rafaelbucard@gmail.com>
2020-12-10 20:23:44 -0300

    Update LoginController.php

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,6 +2,8 @@
 namespace src\controllers;
 
 use \core\Controller;
+use src\handlers\LoginHandler;
+use\src\handlers\LoguinHandeler;
 
 
 class loginController extends Controller {
@@ -11,7 +13,15 @@ class loginController extends Controller {
     }
 
     public function signinAction() {
-        echo 'login recebido';
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password');
+        if ($email && $password) {
+            $token = LoginHandler::verifyLogin($email, $password);
+            
+        } else {
+            $_SESSION['flash'] = 'Digite os campos de e-mail e senha';
+            $this->redirect('login');
+        }
     }
 
     public function signup() {
