51775405fac2cbddddc0b80310a4d3483f5b9212
Rafael Pereira Buçard <rafaelbucard@gmail.com>
2021-01-02 19:49:53 -0300

    Update HomeController.php

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller {
         
 
     public function index() {
-        $this->render('home', ['nome' => 'Bonieky']);
+        $this->render('home', ['nome' => 'Rafael']);
     }
 
 }
