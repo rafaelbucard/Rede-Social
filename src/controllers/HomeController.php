e0226298de0e7a5be7768090d9c47904cba6ed20
bucard <rafaelbucard@gmail.com>
2021-02-19 20:50:59 -0300

    home/header/footer

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller {
 
     public function index() {
         $this->render('home', ['nome' => 'Rafael']);
-        phpinfo () ;
+      
     }
 
 }
---
+++ src/views/partials/header.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/>
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href=""><img src="assets/images/devsbook_logo.png" /></a>
+            </div>
+            <div class="head-side">
+                <div class="head-side-left">
+                    <div class="search-area">
+                        <form method="GET">
+                            <input type="search" placeholder="Pesquisar" name="s" />
+                        </form>
+                    </div>
+                </div>
+                <div class="head-side-right">
+                    <a href="" class="user-area">
+                        <div class="user-area-text">Bonieky</div>
+                        <div class="user-area-icon">
+                            <img src="media/avatars/avatar.jpg" />
+                        </div>
+                    </a>
+                    <a href="" class="user-logout">
+                        <img src="assets/images/power_white.png" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+  
