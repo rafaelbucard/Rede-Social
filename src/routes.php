9c346e73b8294c8196165d3d733fed0780d6fc39
bucard <rafaelbucard@gmail.com>
2021-02-19 23:38:01 -0300

    router

--- src/routes.php
+++ src/routes.php
@@ -4,8 +4,16 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/login', 'LoginController@signin');
 $router->post('/login', 'LoginController@signinAction');
+
 $router->get('/cadastro', 'LoginController@signup');
 $router->post('/cadastro', 'LoginController@signupAction');
+
+//$router->get('/pesquisar);
+//$router->get('/perfil);
+//$router->get('/sair);
+
+
 
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>Social</title>
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/>
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="<?=$base?>/assets/css/style.css" />
 </head>
 <body>
     <header>
@@ -16,20 +16,20 @@
             <div class="head-side">
                 <div class="head-side-left">
                     <div class="search-area">
-                        <form method="GET">
+                        <form method="GET" action="<?=$base?>/pesquisar">
                             <input type="search" placeholder="Pesquisar" name="s" />
                         </form>
                     </div>
                 </div>
                 <div class="head-side-right">
-                    <a href="" class="user-area">
-                        <div class="user-area-text">Bonieky</div>
+                    <a href="<?=$base?>/perfil" class="user-area">
+                        <div class="user-area-text">Rafael</div>
                         <div class="user-area-icon">
-                            <img src="media/avatars/avatar.jpg" />
+                            <img src="<?=$base?>/media/avatars/avatar.jpg" />
                         </div>
                     </a>
-                    <a href="" class="user-logout">
-                        <img src="assets/images/power_white.png" />
+                    <a href="<?=$base?>/sair" class="user-logout">
+                        <img src="<?=$base?>/assets/images/power_white.png" />
                     </a>
                 </div>
             </div>
